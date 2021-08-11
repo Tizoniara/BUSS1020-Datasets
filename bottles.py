e7b7f76b20296e0e9c1502fe22a89fb205092e2f
Edward Liang <edward.liang66@hotmail.com>
2021-08-11 20:40:17 +1000

    fixed error in plotting dates

--- bottles.py
+++ bottles.py
@@ -1,6 +1,7 @@
 import csv
 import sys
 import matplotlib.pyplot as plt
+import datetime
 
 def main():
     with open('bottlescsv.csv') as csv_file:
@@ -11,13 +12,13 @@ def main():
         next(csv_reader)
         for line in csv_reader:
             pair.append(list(line[i] for i in range(2)))
-        print(pair)
         for i in pair:
             x.append(i[0])
             y.append(i[1])
-        print(x)
+        x = [datetime.datetime.strptime(d, "%d/%m/%Y").date() for d in x]
+        print(x[0])
         print(y)
-        plt.plot()
+        plt.plot_date(x,y, linestyle = 'solid', marker = 'x', linewidth = 1)
 
 if __name__ == '__main__':
     print('Running main program')
